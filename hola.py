6e4a566e8cb882bc568bfd2befdd7fe292906ed3
Default user <mp.ramos@uniandes.edu.co>
2019-10-22 17:53:02 +0000

    conteo

--- hola.py
+++ hola.py
@@ -1,1 +1,12 @@
 print('hola mundo')
+#cat muestra contenido
+#status vuelve un directorio un repositorio
+#git init recuerda lo que esta pasando en esa carpeta 
+#voy a agregar la version de hola.py donde solo tenia hola mundo (git add)
+#git status reconoce el archivo nuevo 
+#necesito una ideantidad asociada a un correo electronico (git config --global user.email"mi correo"
+# commit transaccion de guardarlo (git commit -m "primera version"
+
+for i in range(10):
+    print(i)
+    
